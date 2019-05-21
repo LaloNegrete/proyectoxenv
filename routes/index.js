fb7606bbc9da756a1ee90f3a83429024beed953e
unknown <enano.3455@gmail.com>
2019-05-21 15:57:00 -0500

    BS 4

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/hola', function(req, res, next) {
+	//solicitud a una ruta del apirest
+  res.render('saludo', { otraCosa: 'cosa' });
+});
 
 module.exports = router;
