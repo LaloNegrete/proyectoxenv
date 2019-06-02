65559a45d28e28e9376282604bb94654743c8e98
unknown <enano.3455@gmail.com>
2019-06-01 19:24:11 -0500

    cambios al index

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema =  Schema({
+  id:Number,
+  name:String,
+  lastname:String,
+  age:Number
+  born: Number,
+  death: Number,
+  movement: String,
+  place_of_birth: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
