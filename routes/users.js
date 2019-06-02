bf199951ddb039bf6a990bb5fdd68ed6eeaf8b0a
unknown <enano.3455@gmail.com>
2019-06-01 20:53:07 -0500

    cambios en el git

--- routes/users.js
+++ routes/users.js
@@ -63,5 +63,21 @@ router.delete('/', function(req, res, next){
   res.status(405).json({mensaje: 'Accion no permitida'});
 });
 
+router.patch('/:userId', function(req,res,next){
+  user.findOneAndUpdate({
+    id: req.params.userId
+  }, function(err, data){
+    if(err){
+      res.status(404).json(err);
+      mensaje: 'No encontrado'
+    }
+    res.status(200).json(data);
+    mensaje: 'OK'
+  });
+});
+router.patch('/:userId', function(req,res, next){
+  res.status(405).json({mensaje: 'Accion no permitida.'});
+});
+
 module.exports = router;
 
